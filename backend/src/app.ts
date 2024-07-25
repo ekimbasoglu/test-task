a08c0299ba314b583c567797da302311130f4fbc
Ekim <ekimbasoglu@hotmail.com>
2024-07-25 17:18:16 +0300

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import moviesRoutes from "./routes/moviesRoutes";
 import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
+app.use(cors()); // Use cors middleware
 app.use(express.json());
 app.use("/api/movies", moviesRoutes);
 
-// Use the error handling middleware
 app.use(errorHandler);
 
 export default app;
